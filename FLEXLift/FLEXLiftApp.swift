7eadf37c7f1fa2fa01a2f0fde4d37711ff049749
Ava <avapardo@umich.edu>
2023-11-07 23:36:31 -0500

    Initial Commit

---
+++ FLEXLift/FLEXLiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  FLEXLiftApp.swift
+//  FLEXLift
+//
+//  Created by Ava Luna Pardo Keegan on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct FLEXLiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
