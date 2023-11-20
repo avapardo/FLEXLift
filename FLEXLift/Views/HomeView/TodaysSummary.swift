118245595f5d290884dab646a7bda79006b9911e
Ava Pardo <avapardo@Avas-iMac.local>
2023-11-19 23:10:12 -0500

    Today's Summary UI

---
+++ FLEXLift/Views/HomeView/TodaysSummary.swift
@@ -0,0 +1,18 @@
+//
+//  TodaysSummary.swift
+//  FLEXLift
+//
+//  Created by Ava Pardo on 11/19/23.
+//
+
+import SwiftUI
+
+struct TodaysSummary: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodaysSummary()
+}
